060c7948a38ed0265395b008c9a283c027ed618c
Hector Jose Flores Colmenarez <hecto932@gmail.com>
2015-04-20 23:04:03 -0430

    Clase 1 de Django, sin South

---
+++ tracks/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from artists.models import Artist 
+from albums.models import Album
+
+class Track(models.Model):
+	title = models.CharField(max_length=255)
+	order = models.PositiveIntegerField()
+	track_file = models.FileField(upload_to='tracks')
+	album = models.ForeignKey(Album)
+	artist = models.ForeignKey(Artist)
